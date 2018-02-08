b082acd7164ccb07524b5d68883c8ab9c5cc145c
Andrew Jose <andrewjbcbs@gmail.com>
2018-02-09 01:53:01 +0300

    Updated to CefSharp 63 WinForms

---
+++ src/packages/CefSharp.Common.63.0.0/src/CefSharp.Core/Internals/CookieVisitor.cpp
@@ -0,0 +1,35 @@
+// Copyright © 2010-2017 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#include "Stdafx.h"
+#include "CookieVisitor.h"
+
+namespace CefSharp
+{
+    bool CookieVisitor::Visit(const CefCookie& cefCookie, int count, int total, bool& deleteCookie)
+    {
+        auto cookie = gcnew Cookie();
+        String^ cookieName = StringUtils::ToClr(cefCookie.name);
+
+        if (!String::IsNullOrEmpty(cookieName))
+        {
+            cookie->Name = StringUtils::ToClr(cefCookie.name);
+            cookie->Value = StringUtils::ToClr(cefCookie.value);
+            cookie->Domain = StringUtils::ToClr(cefCookie.domain);
+            cookie->Path = StringUtils::ToClr(cefCookie.path);
+            cookie->Secure = cefCookie.secure == 1;
+            cookie->HttpOnly = cefCookie.httponly == 1;
+
+            if (cefCookie.has_expires)
+            {
+                cookie->Expires = DateTimeUtils::FromCefTime(CefTime(cefCookie.expires).GetDoubleT());
+            }
+
+            cookie->Creation = DateTimeUtils::FromCefTime(CefTime(cefCookie.creation).GetDoubleT());
+            cookie->LastAccess = DateTimeUtils::FromCefTime(CefTime(cefCookie.last_access).GetDoubleT());
+        }
+
+        return _visitor->Visit(cookie, count, total, deleteCookie);
+    }
+}
---
+++ src/packages/CefSharp.Common.63.0.0/src/CefSharp/Cookie.cs
@@ -0,0 +1,28 @@
+﻿// Copyright © 2010-2017 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
+using System.Diagnostics;
+
+namespace CefSharp
+{
+    /// <summary>
+    /// Class used to Represent a cookie the built in .Net Cookie
+    /// class isn't used as some of it's properties have internal setters
+    /// </summary>
+    [DebuggerDisplay("Domain = {Domain}, Path = {Path}, Name = {Name}, Secure = {Secure}, HttpOnly = {HttpOnly}," +
+                     "Creation = {Creation}, Expires = {Expires}, LastAccess = {LastAccess}", Name = "Cookie")] 
+    public sealed class Cookie
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Domain { get; set; }
+        public string Path { get; set; }
+        public bool Secure { get; set; }
+        public bool HttpOnly { get; set; }
+        public DateTime? Expires { get; set; }
+        public DateTime Creation { get; set; }
+        public DateTime LastAccess { get; set; }
+    }
+}
