4f3d7b0fedd9f13227e42dc4f883e37a102c37bb
Andrew Jose <andrewjbcbs@gmail.com>
2018-03-01 02:04:17 +0300

    Fixed User agent, changed branding to "flamesky://", and I fixed a bug which was present in History.

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FlameSky.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -57,6 +57,18 @@ namespace FlameSky.Properties {
         public global::System.Collections.Specialized.StringCollection URLHistory {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["URLHistory"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string BrowserHistoryFilepath {
+            get {
+                return ((string)(this["BrowserHistoryFilepath"]));
+            }
+            set {
+                this["BrowserHistoryFilepath"] = value;
             }
         }
     }
